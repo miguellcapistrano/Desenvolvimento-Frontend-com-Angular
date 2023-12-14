1ab7f4e301fbfe3134a377ffe014c17ef54797b9
miguellcapistrano <luizmiguell9322@gmail.com>
2023-12-13 21:40:53 -0300

    Desafio Primeiros Passos com JS

---
+++ Primeiros Passos com JS/Desafio1.js
@@ -0,0 +1,8 @@
+let precoCombustivel = 5.55
+let kmPorLitro = 2
+let distanciaDaViagem = 100
+
+let gastoFinal = distanciaDaViagem / kmPorLitro * precoCombustivel
+
+console.log(`O valor do combustível para a viagem será de ${gastoFinal} reais`)
+
